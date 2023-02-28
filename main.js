caf231fcab20837c1895e2f33110d0df0f055378
MalikaRavshanova <malikaravshanova1234@gmail.com>
2023-02-28 07:44:41 +0500

    intial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// let k = 10
+// let n = 5
+// if(n > 0){
+//     for(let i = 0; i<n; i++){
+//         console.log(k)
+//     }
+// } 
+let a = 20
+let b = 10  
+if(a < b){
+    for(let i =0; i<b; i++){
+        console.log(a)
+    }
+}
